4bedfc0504c43f14cc04bb83324cd89aadffb618
Michial Andrew Green II <vmichial@gmail.com>
2013-10-04 22:02:25 -0700

    school complete, working on player
    the data for school functionality is complete

--- Assets/Libs/sudanClasses.js
+++ Assets/Libs/sudanClasses.js
@@ -3,8 +3,7 @@
 //a disease will have a name, a chance of happening, and a cost
 //a person will give themselves a disease on certain conditions
 function Disease(ParentObj){
-	this.parent = ParentObj;
-	
+	this.parent = ParentObj;	
 	//is the disease still active
 	this.active = true;
 	//name of the disease
@@ -27,26 +26,43 @@ function Disease(ParentObj){
 }
 
 //life events have a name
-function LifeEvent(){
+function LifeEvent(ParentObj){
+	this.parent = ParentObj;
+	this.effect;
+	this.active = false;
+	this.chance;
+	this.increaceChance = function(increaseBy){this.chance+=increaseBy;}
+	
+}
+
+function Disaster(ParentObj){
+	this.name = "Flood";
+	this.effect = "Your Area has experienced a flood";
+	this.continuingEffect = "The flood continues...";
+	this.noHospital = true;
+	this.noSchool = true;
+	this.noWork = true;
+	this.duration = 3;	
+}
+
+function Aid(ParentObj){
 	
 }
 
 //Person is a base class. All peple inherit from it
-function Person(){
-	this.gender = 'female';
+function Player(){
 	this.married = false;
 	//if the family member is alive, yay
 	this.alive = true;
 	//is the person capable of working
 	this.canWork = true;
-	
+	//is the player capable of schooling
+	this.canSchool = true;	
 	//age can be a number between 5 and 60
-	this.Age = 30;
+	this.age = 5;
 	//education is a number between 1 and 12 (inclusive) 
 	//it tells grade level
-	this.Education = 1;
-	//progress goes from [0,100] for how much work done
-	this.workProgress = 0;
+	this.education = 1;
 	//progress goes from [0,100] for how much schooling done
 	this.schoolProgress = 0;
 	//the player will need to make diseases based on some die
@@ -58,10 +74,18 @@ function Person(){
 	this.lifeEvent = new Array();
 	//init is blank, it is here for initialization
 	this.init = function(){
-	
+		this.married = false;
+		this.alive = true;
+		this.canWork = true;
+		this.canSchool = true;
+		this.schoolProgress = true;
+		this.diseases = new Array();
+		this.lifeEvent = new Array();
+		this.age = 5;
+		this.education = 1;
 	}
-	this.handleTurn = function(){
-	
+	this.checkDiseases = function(){
+		
 	}
 }
 
@@ -69,6 +93,7 @@ function Person(){
 function Father(){
 	this.alive = true;
 	this.canWork = true;
+	this.canSchool = false;
 	this.diseases = new Array();
 	this.lifeEvents = new Array();
 }
@@ -77,18 +102,63 @@ function Father(){
 function Mother(){
 	this.alive = true;
 	this.canWork = true;
+	this.canSchool = false;
 	this.diseases = new Array();
 	this.lifeEvents = new Array();
 }
 
-//Daughter is the player type, she is a person but will have special
-//educational goals
-function Daughter(){
-	
+function Sibling(){
+	this.alive = true;
+	this.canWork = true;
+	this.canSchool =true;
+	this.diseases = new Array();
+	this.lifeEvents = new Array();
 }
-Daughter.prototype = new Person();
+// the school is used for certai education bonuses
+//use roll to initialize the school randomly
+function School(){
+	this.available = true;
+	this.hasGoodTeacher = false;;
+	this.hasBuilding = false;
+	this.hasMats = false;
+	this.feedStudents = false;
+	this.far = false;
+	
+	this.roll = function(){
+		var rolls = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+		this.far = ( (roll<50)? true : false );
+		rolls = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+		this.feedStudents = ( (roll<13) ?  true : false );
+		rolls = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+		this.hasMats = ( (roll<11) ? true : false );
+		rolls = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+		this.hasGoodTeacher = ( (roll<51) ? true : false );
+		rolls = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+		this.hasBuilding = ( (roll<16) ? true : false);
+	}
+}
+
+function Hospital(){
+	available = false;
+	this.roll = function(){
+		rolls = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+		this.available = ( (rolls<6)? true : false );
+	}
+}
+
+function Family(){
+	this.school = new School();
+	this.father = new Father();
+	this.mother = new Mother();
+}
 
 function GameState(){
-	this.handleTurn = false;
-	this.MakeDecisions = true;
+	this.start = true;
+	this.choose = false;
+	this.goTo = false;
+	this.atWork = false;
+	this.rollDiseases = false;
+	this.rollDisasters = false;
+	this.rollAid = false;
+	this.victoryCheck = false;
 }
