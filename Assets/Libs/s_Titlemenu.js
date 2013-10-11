1cbee73e30899fd965b49987f254170578c0dc72
Edward Thang <RandomClown@gmail.com>
2013-10-10 23:27:07 -0700

    added credits button

--- Assets/Libs/s_Titlemenu.js
+++ Assets/Libs/s_Titlemenu.js
@@ -6,22 +6,31 @@
 function titlemenu_init(){
 	// Make the components
 	var ent_bg=new Entity('ent_bg');
-	var ent_start=new Entity('ent_start');
 	var tex_bg=new Texture('tex_bg', 'bgtitle');
-	var tex_start=new Texture('tex_start', 'btnstart');
-	
 	ent_bg.addTex(tex_bg);
+
+	var ent_start=new Entity('ent_start');
+	var tex_start=new Texture('tex_start', 'btnstart');
 	ent_start.addTex(tex_start);
+	
+	var ent_credits=new Entity('ent_credits');
+	var tex_credits=new Texture('tex_credits', 'btncredits');
+	ent_credits.addTex(tex_credits);
 	
 	this.addEnt(ent_bg);
 	this.addEnt(ent_start);
+	this.addEnt(ent_credits);
 	
 	// Configure components
 	tex_bg.mid={x:0, y:0};
 	
 	ent_start.hitbox={x:tex_start.size.x, y:tex_start.size.y};
-	ent_start.pos={x:canvas.width*4/5, y:canvas.height*3/4};
+	ent_start.pos={x:canvas.width*4/5, y:canvas.height*2/4};
 	ent_start.scale={x:.5, y:.5};
+	
+	ent_credits.hitbox={x:tex_credits.size.x, y:tex_credits.size.y};
+	ent_credits.pos={x:canvas.width*4/5, y:canvas.height*3/4};
+	ent_credits.scale={x:.5, y:.5};
 	
 	this.vel=64;
 	
